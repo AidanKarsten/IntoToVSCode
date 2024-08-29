08c909c273d483f04676f9479d0386101d701e5e
Aidan Karsten <Aidan_Karsten1@baylor.edu>
2024-08-29 15:10:07 -0500

    Initial Commit

---
+++ MyGraph.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+#step 1: create a virutal environment (VE)
+    #python3-m venv myvenv (name is subject to change for VE)
+#step 2: activate your VE
+    #source myvenv/bin/activate
+#step 3: install 3rd party library
+    #pip3 install matplotlib (name of library)
+#version control is everything related to your project: pics, docs etc.
+#source control only refers to the source code
---
+++ python1.py
@@ -0,0 +1,11 @@
+a = 1 
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c   #when using break point in bebugging, code runs up to that line, not including it
+#if break point is in loop, code will debug as long as the loop is valid
+
+message = 'Hello World!'
+
+print(message)
